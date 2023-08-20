a761b1b909813c8586565036a4a174fa16b99b8a
Paweł Rybak <pawel.rybak13@gmail.com>
2023-08-20 17:38:13 +0200

    Added some constants

--- chacha20.go
+++ chacha20.go
@@ -1,5 +1,15 @@
 package chacha20
 
-func Hello() string {
-	return "hello"
-}
+const (
+	BLOCK_SIZE = 64
+	WORD_SIZE  = 4
+	KEY_SIZE   = 32
+)
+
+// "expand 32-byte k"
+const (
+	CONSTANT_0 = uint32(0x61707865)
+	CONSTANT_1 = uint32(0x3320646e)
+	CONSTANT_2 = uint32(0x79622d32)
+	CONSTANT_3 = uint32(0x6b206574)
+)
