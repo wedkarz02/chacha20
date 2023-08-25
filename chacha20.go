adf3afc84b62caa99c4ce8e7e514a171352c4eaf
Paweł Rybak <pawel.rybak13@gmail.com>
2023-08-25 20:09:48 +0200

    Added a LICENSE notice to source files

--- chacha20.go
+++ chacha20.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2023 Paweł Rybak
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package chacha20
 
 import (
@@ -115,7 +135,7 @@ func (c *Cipher) block() {
 	var initialState [STATE_SIZE]uint32
 	copy(initialState[:], c.state[:])
 
-	// 20 rounds of alternating column rounds and diagonal rounds
+	// 20 rounds of alternating column rounds and diagonal rounds.
 	for i := 0; i < NR/2; i++ {
 		// Column round
 		c.quarterRound(0, 4, 8, 12)
@@ -130,6 +150,7 @@ func (c *Cipher) block() {
 		c.quarterRound(3, 4, 9, 14)
 	}
 
+	// Adding the initial state using mod 2^32 addition.
 	for i, word := range initialState {
 		c.state[i] += word
 	}
